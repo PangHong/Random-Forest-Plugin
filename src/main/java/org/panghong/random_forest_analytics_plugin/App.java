856b79e1d5099ff60b660180177f5b075f98ef52
Pang Hong <2014220902023@std.uestc.edu.cn>
2018-03-13 09:51:34 +0800

    Init Random Forest Analytics Plugin Project, and change pom.xml that copy from decision tree analytics plugin

---
+++ src/main/java/org/panghong/random_forest_analytics_plugin/App.java
@@ -0,0 +1,13 @@
+package org.panghong.random_forest_analytics_plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
